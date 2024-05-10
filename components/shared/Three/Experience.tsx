5d97b63636e80b3566a642c69687ad41a30898b0
ywx <540040588@qq.com>
2024-05-10 17:51:46 +0800

    add animations

--- components/shared/Three/Experience.tsx
+++ components/shared/Three/Experience.tsx
@@ -1,25 +1,62 @@
 "use client";
 
-import { OrbitControls } from "@react-three/drei";
+import {
+  ContactShadows,
+  Environment,
+  OrbitControls,
+  Sky,
+} from "@react-three/drei";
 import React from "react";
 import { YwxModel } from "./Ywx";
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import { useControls } from "leva";
 
 const Experience = () => {
   return (
     <Canvas
       style={{ width: "600px", height: "600px" }}
       shadows
-      camera={{ position: [0, 4, 3], fov: 30 }}
+      camera={{ position: [0, 1, 4], fov: 30 }}
     >
+      <color attach="background" args={["#ececec"]} />
       <OrbitControls />
+      <Sky />
+      <Environment preset="warehouse" environmentIntensity={0.9} />
       <group position-y={-1}>
+        <ContactShadows
+          opacity={0.42}
+          scale={10}
+          blur={1}
+          far={10}
+          resolution={256}
+          color="#000000"
+        />
         <YwxModel />
       </group>
-
-      <ambientLight intensity={1} />
+      {/* <Arena /> */}
     </Canvas>
   );
 };
+
+function Arena() {
+  const { camera } = useThree();
+  const {
+    x: CameraX,
+    y: CameraY,
+    z: CameraZ,
+  } = useControls("Camera Position", {
+    x: { value: 0, step: 1 },
+    y: { value: 1, step: 1 },
+    z: { value: 4, step: 1 },
+  });
+
+  useFrame(() => {
+    camera.position.x = CameraX;
+    camera.position.y = CameraY;
+    camera.position.z = CameraZ;
+  });
+
+  return <></>;
+}
 
 export default Experience;
